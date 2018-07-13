afc695a0f252f0ff71a95b8307108923f4be1741
danielemasini <danielemasini@cb9afbb8-42ac-48c3-b28b-00f929f7777a>
2018-07-13 19:36:19 +0000

    inserimento riquadro conclusivo

--- trunk/Matemapp/src/com/firenze1985/matemapp/aritmetica/insiemi/InsiemiCap1.java
+++ trunk/Matemapp/src/com/firenze1985/matemapp/aritmetica/insiemi/InsiemiCap1.java
@@ -39,39 +39,43 @@ public class InsiemiCap1 extends Argomento {
 		mappaPartiInGrassetto.put(54, 68);
 		mappaPartiInGrassetto.put(110, 127);
 		inserisciDefinizione("Un insieme è una collezione di elementi che hanno una caratteristica in comune, in cui possiamo stabilire con assoluta certezza se un elemento appartiene o meno all'insieme", mappaPartiInGrassetto);
-		inserisciTestoConParteInCorsivo("Facciamo un attimo attenzione alla seconda parte della definizione, quella relativa alla assoluta certezza",
+		inserisciTestoConParteInGrassetto("Facciamo un attimo attenzione alla seconda parte della definizione, quella relativa alla assoluta certezza",
 				89, 106);
 		inserisciTesto("Che significa? Facciamo un esempio per capire meglio: supponi che il vostro insegnante chieda a ognuno di voi di creare l'insieme dei piatti preferiti.");
 		inserisciTesto("Cosa capiterà? Che tu, magari, avrai inserito alcuni piatti che alcuni tuoi compagni invece non hanno inserito perchè per il loro gusto non sono piacevoli. E viceversa, loro potranno aver inserito piatti che a te non piaccono per niente!");
 		inserisciTesto("Chi ha ragione? Tu? Loro?");
-		inserisciTesto("In realtà...nessuno! Questo perchè l'esercizio proposto contiene un errore di fondo: l'insieme dei piatti preferiti è soggettivo. Pertanto ognuno di noi creerà una lista differente dagli altri.");
-		inserisciTesto("Più tecnicamente, si dice che la caratteristica essere un piatto preferito è soggettiva. "
+		inserisciTesto("In realtà...nessuno! Questo perchè l'esercizio proposto contiene un errore di fondo: l'insieme dei piatti preferiti non è uguale per tutti. Pertanto ognuno di noi creerà una lista differente dagli altri.");
+		inserisciTestoConParteInGrassetto("Più tecnicamente, si dice che la caratteristica essere un piatto preferito è soggettiva. "
 				+ "Ovvero, ognuno di noi ha i suoi piatti preferiti, in base ai propri gusti. "
 				+ "Quindi questa caratteristica fa cambiare, da soggetto a soggetto (ecco perchè si dice soggettiva), "
-				+ "la costruzione della collezione di elementi");
+				+ "la costruzione della collezione di elementi", 77, 87);
 		inserisciTesto("Ecco che quindi non abbiamo creato un insieme.");
 		inserisciStacco();
 		inserisciTesto("Se invece il tuo insegnante vi chiede di costruire l'insieme di tutte le regioni italiane...cosa succede?");
 		inserisciTesto("Stavolta tu e tutti i tuoi compagni avrete creato lo stesso insieme.");
-		inserisciTesto("Questo perchè la caratteristica essere o meno una regione italiana è oggettiva. Ovvero, ogni persona fornisce la stessa risposta");
+		inserisciTestoConParteInGrassetto("Questo perchè la caratteristica essere o meno una regione italiana è oggettiva. "
+				+ "Ovvero, ogni persona fornisce la stessa risposta", 69,78);
 		inserisciStacco();
-		inserisciTesto("Come presto imparerai, in matematica siamo pigri. Non abbiamo molta voglia di scrivere. Ecco che una volta capito un concetto, dobbiamo imparare anche alcuni simboli che ci permettono di accorciare le frasi.");
-		inserisciTesto("Immagina di dover scrivere ad esempio: la Toscana appartiene all'insieme delle regioni italiane, la Puglia appartiene all'insieme delle regioni italiane...non finiremmo più!");
+		inserisciTestoConParteInGrassetto("Come presto imparerai, in matematica siamo pigri. Non abbiamo molta voglia di scrivere. "
+				+ "Ecco che una volta capito un concetto, dobbiamo imparare alcuni simboli che ci permettono di accorciare le frasi.", 136,159);
+		inserisciTesto("Immagina di dover scrivere ad esempio: la Toscana appartiene all'insieme delle regioni italiane, la Puglia appartiene all'insieme delle regioni italiane, e così via. Non finiremmo più!");
 		inserisciTesto("Dobbiamo inventarci qualcosa di furbo: ecco che ci vengono in aiuto una serie di convenzioni e simboli");
-		inserisciTesto("Innanzitutto gli insiemi si indicano solitamente con una lettera maiuscola dell'alfabeto: A, B, C,...");
-		inserisciTesto("Gli elementi di un insieme si indicano solitamente con una lettera minuscola dell'alfabeto: a, b, c, ....");
+		inserisciStacco();
+		inserisciTestoConParteInGrassetto("Innanzitutto gli insiemi si indicano solitamente con una lettera maiuscola dell'alfabeto: A, B, C,...", 57, 89);
+		inserisciTestoConParteInGrassetto("Gli elementi di un insieme si indicano solitamente con una lettera minuscola dell'alfabeto: a, b, c, ....", 59, 90);
 		inserisciTesto("Se quindi indichiamo, ad esempio, con t la Toscana e con R l'insieme delle regioni italiane, per dire che la Toscana appartiene all'insieme delle regioni italiane ci basta semplicemente scrivere quanto segue:");
 		mappaPartiInGrassetto = new HashMap<Integer, Integer>();
 		mappaPartiInGrassetto.put(0, 0);
 		inserisciDefinizione("t \u2208 R", mappaPartiInGrassetto);
 		inserisciTesto("Tre simboli per dire una frase piuttosto lunga. Comodo no?");
-		inserisciTestoConParteInGrassetto("Abbiamo imparato il simbolo di appartenenza", 31, 43);
+		inserisciTestoConParteInGrassetto("Abbiamo imparato il simbolo di appartenenza.", 31, 43);
 		inserisciTesto("Se invece volessimo dire che la Juventus non appartiene all'insieme delle regioni italiane?");
 		mappaPartiInGrassetto = new HashMap<Integer, Integer>();
 		mappaPartiInGrassetto.put(0, 0);
 		inserisciDefinizione("j \u2209 R", mappaPartiInGrassetto);
-		inserisciTestoConParteInGrassetto("Questo è invece il simbolo di non appartenenza", 30, 46);
-		
+		inserisciTestoConParteInGrassetto("Questo è invece il simbolo di non appartenenza.", 30, 46);
+		inserisciStacco();
+		inserisciTestoConRiquadroConclusivo("- hai imparato la definizione di insieme\n\n- hai imparato come scrivere in simboli matematici se un elemento appartiene o no a un insieme");
 		return getAndroidView();
 
 	}
